c4d7543642c9332c8c8496a68034b59a2ac6e26e
nguyenduykhang0208 <79954193+nguyenduykhang0208@users.noreply.github.com>
2021-11-20 10:31:24 +0700

    Update Coffee.h

--- Drink/Coffee.h
+++ Drink/Coffee.h
@@ -125,6 +125,6 @@ void Coffee::addDrink(int choose, int numberOf)
 
 void Coffee::print()
 {	
-    cout <<setw(20)<<name <<setw(20)<< quantity <<setw(20)<< unitPrice <<setw(20)<<amount<<endl;
+    cout <<setw(30)<<name <<setw(20)<< quantity <<setw(20)<< unitPrice <<setw(20)<<amount<<endl;
     cout << endl;
 }
