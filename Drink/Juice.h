8593e99e2d16696329f9d0c8b19946a6313baf1c
nguyenduykhang0208 <79954193+nguyenduykhang0208@users.noreply.github.com>
2021-11-20 10:32:39 +0700

    Update Juice.h

--- Drink/Juice.h
+++ Drink/Juice.h
@@ -103,6 +103,6 @@ void Juice::addDrink(int choose, int numberOf)
 
 void Juice::print()
 {
-    cout <<setw(20)<<name <<setw(20)<< quantity <<setw(20)<< unitPrice <<setw(20)<<amount<<endl;
+    cout <<setw(30)<<name <<setw(20)<< quantity <<setw(20)<< unitPrice <<setw(20)<<amount<<endl;
     cout << endl;
 }
