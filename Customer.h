5371f7bd326a1ac1fe695ed851f2c823f3d83a64
nguyenduykhang0208 <79954193+nguyenduykhang0208@users.noreply.github.com>
2021-12-10 19:34:00 +0700

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -13,13 +13,11 @@
 
 using namespace std;
 
-class Customer
+class Table
 {
 private:
-    int totalPrice; //Lưu giữ tổng số tiền cho số đồ uống đã gọi
-
-    //ĐÂY LÀ CÁC BIẾN DÙNG ĐỂ LƯU GIỮ GIÁ TRỊ CHO TỪNG THỨC UỐNG
-
+    double totalPrice;                                                                     //Lưu giữ tổng số tiền cho số đồ uống đã gọi
+                                                                                           //ĐÂY LÀ CÁC BIẾN DÙNG ĐỂ LƯU GIỮ GIÁ TRỊ CHO TỪNG THỨC UỐNG
     Drink *milkTea, *gingerTea, *kumquatTea, *lemonTea, *liptonTea, *lycheeTea, *peachTea; //Tea (7)
 
     Drink *blackCoffee, *milkCoffee, *whiteCoffee, *hotCocoa, *espresso, *latte, *cappuccino, *hotChocolate; //Coffee (8)
@@ -36,6 +34,22 @@ private:
 
     Drink *sevenUp, *cocaCola, *redbull, *revive, *sting, *c2; //Soft Drink (6)
 
+    Tea tempTea;
+
+    Coffee tempCoffee;
+
+    IceBlended tempiceBlended;
+
+    Juice tempJuice;
+
+    Smoothie tempSmoothie;
+
+    Soda tempSoda;
+
+    SoftDrink tempSoftDrink;
+
+    Yogurt tempYogurt;
+
     //Các hàm được gọi ra cho các loại đồ uống
     void chooseTea(int thisDrink, int numberOf);
     void chooseCoffee(int thisDrink, int numberOf);
@@ -47,8 +61,8 @@ private:
     void chooseSoftDrink(int thisDrink, int numberOf);
 
 public:
-    Customer();
-    ~Customer();
+    Table();
+    ~Table();
 
     int getTotalPrice() { return totalPrice; }
     void callDrink(int typeOfDrink, int thisDrink, int numberOf); //Hàm nhập đồ uống thông qua các thao tác
@@ -57,10 +71,10 @@ public:
     void removeAllDrink();
 };
 
-int Customer::totalPrice = 0;
-
-Customer::Customer()
+Table::Table()
 {
+    totalPrice = 0;
+
     milkTea = new Tea;
     gingerTea = new Tea;
     kumquatTea = new Tea;
@@ -122,7 +136,7 @@ Customer::Customer()
     c2 = new SoftDrink;
 }
 
-Customer::~Customer()
+Table::~Table()
 {
     delete milkTea;
     delete gingerTea;
@@ -185,7 +199,7 @@ Customer::~Customer()
     delete c2;
 }
 
-void Customer::callDrink(int typeOfDrink, int thisDrink, int numberOf)
+void Table::callDrink(int typeOfDrink, int thisDrink, int numberOf)
 {
     switch (typeOfDrink)
     {
@@ -216,7 +230,7 @@ void Customer::callDrink(int typeOfDrink, int thisDrink, int numberOf)
     }
 }
 
-void Customer::removeDrink(int typeOfDrink, int thisDrink)
+void Table::removeDrink(int typeOfDrink, int thisDrink)
 {
     switch (typeOfDrink)
     {
@@ -247,269 +261,332 @@ void Customer::removeDrink(int typeOfDrink, int thisDrink)
     }
 }
 
-void Customer::chooseTea(int thisDrink, int numberOf)
+void Table::chooseTea(int thisDrink, int numberOf)
 {
+    if (numberOf == 0)
+    {
+        milkTea->addDrink(1, 0);
+        gingerTea->addDrink(1, 0);
+        kumquatTea->addDrink(1, 0);
+        lemonTea->addDrink(1, 0);
+        liptonTea->addDrink(1, 0);
+        lycheeTea->addDrink(1, 0);
+        peachTea->addDrink(1, 0);
+        return;
+    }
     switch (thisDrink)
     {
     case 1:
-        milkTea->addDrink(1, numberOf);
-        totalPrice += milkTea->getAmount();
+        tempTea.addDrink(1, numberOf);
+        *milkTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     case 2:
-        gingerTea->addDrink(2, numberOf);
-        totalPrice += gingerTea->getAmount();
+        tempTea.addDrink(2, numberOf);
+        *gingerTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     case 3:
-        kumquatTea->addDrink(3, numberOf);
-        totalPrice += kumquatTea->getAmount();
+        tempTea.addDrink(3, numberOf);
+        *kumquatTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     case 4:
-        lemonTea->addDrink(4, numberOf);
-        totalPrice += lemonTea->getAmount();
+        tempTea.addDrink(4, numberOf);
+        *lemonTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     case 5:
-        liptonTea->addDrink(5, numberOf);
-        totalPrice += liptonTea->getAmount();
+        tempTea.addDrink(5, numberOf);
+        *liptonTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     case 6:
-        lycheeTea->addDrink(6, numberOf);
-        totalPrice += lycheeTea->getAmount();
+        tempTea.addDrink(6, numberOf);
+        *lycheeTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     case 7:
-        peachTea->addDrink(6, numberOf);
-        totalPrice += peachTea->getAmount();
+        tempTea.addDrink(6, numberOf);
+        *peachTea += tempTea;
+        totalPrice += tempTea.getAmount();
         break;
     }
 }
 
-void Customer::chooseCoffee(int thisDrink, int numberOf)
+void Table::chooseCoffee(int thisDrink, int numberOf)
 {
+
     switch (thisDrink)
     {
     case 1:
-        blackCoffee->addDrink(1, numberOf);
-        totalPrice += blackCoffee->getAmount();
+        tempCoffee.addDrink(1, numberOf);
+        *blackCoffee += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 2:
-        milkCoffee->addDrink(2, numberOf);
-        totalPrice += milkCoffee->getAmount();
+        tempCoffee.addDrink(2, numberOf);
+        *milkCoffee += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 3:
-        whiteCoffee->addDrink(3, numberOf);
-        totalPrice += whiteCoffee->getAmount();
+        tempCoffee.addDrink(3, numberOf);
+        *whiteCoffee += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 4:
-        hotCocoa->addDrink(4, numberOf);
-        totalPrice += hotCocoa->getAmount();
+        tempCoffee.addDrink(4, numberOf);
+        *hotCocoa += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 5:
-        espresso->addDrink(5, numberOf);
-        totalPrice += espresso->getAmount();
+        tempCoffee.addDrink(5, numberOf);
+        *espresso += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 6:
-        latte->addDrink(6, numberOf);
-        totalPrice += latte->getAmount();
+        tempCoffee.addDrink(6, numberOf);
+        *latte += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 7:
-        cappuccino->addDrink(7, numberOf);
-        totalPrice += cappuccino->getAmount();
+        tempCoffee.addDrink(7, numberOf);
+        *cappuccino += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     case 8:
-        hotChocolate->addDrink(8, numberOf);
-        totalPrice += hotChocolate->getAmount();
+        tempCoffee.addDrink(8, numberOf);
+        *hotChocolate += tempCoffee;
+        totalPrice += tempCoffee.getAmount();
         break;
     }
 }
 
-void Customer::chooseJuice(int thisDrink, int numberOf)
+void Table::chooseJuice(int thisDrink, int numberOf)
 {
     switch (thisDrink)
     {
     case 1:
-        lemonade->addDrink(1, numberOf);
-        totalPrice += lemonade->getAmount();
+        tempJuice.addDrink(1, numberOf);
+        *lemonade += tempJuice;
+        totalPrice += tempJuice.getAmount();
         break;
     case 2:
-        orangeade->addDrink(2, numberOf);
-        totalPrice += orangeade->getAmount();
+        tempJuice.addDrink(2, numberOf);
+        *orangeade += tempJuice;
+        totalPrice += tempJuice.getAmount();
         break;
     case 3:
-        watermelon_Juice->addDrink(3, numberOf);
-        totalPrice += watermelon_Juice->getAmount();
+        tempJuice.addDrink(3, numberOf);
+        *watermelon_Juice += tempJuice;
+        totalPrice += tempJuice.getAmount();
         break;
     case 4:
-        passionFruit_Juice->addDrink(4, numberOf);
-        totalPrice += passionFruit_Juice->getAmount();
+        tempJuice.addDrink(4, numberOf);
+        *passionFruit_Juice += tempJuice;
+        totalPrice += tempJuice.getAmount();
         break;
     case 5:
-        carrot_Juice->addDrink(5, numberOf);
-        totalPrice += carrot_Juice->getAmount();
+        tempJuice.addDrink(5, numberOf);
+        *carrot_Juice += tempJuice;
+        totalPrice += tempJuice.getAmount();
         break;
     case 6:
-        coconutWater->addDrink(6, numberOf);
-        totalPrice += coconutWater->getAmount();
+        tempJuice.addDrink(6, numberOf);
+        *coconutWater += tempJuice;
+        totalPrice += tempJuice.getAmount();
         break;
     }
 }
 
-void Customer::chooseSmoothie(int thisDrink, int numberOf)
+void Table::chooseSmoothie(int thisDrink, int numberOf)
 {
     switch (thisDrink)
     {
     case 1:
-        avocado_Smoothie->addDrink(1, numberOf);
-        totalPrice += avocado_Smoothie->getAmount();
+        tempSmoothie.addDrink(1, numberOf);
+        *avocado_Smoothie += tempSmoothie;
+        totalPrice += tempSmoothie.getAmount();
         break;
     case 2:
-        durian_Smoothie->addDrink(2, numberOf);
-        totalPrice += durian_Smoothie->getAmount();
+        tempSmoothie.addDrink(2, numberOf);
+        *durian_Smoothie += tempSmoothie;
+        totalPrice += tempSmoothie.getAmount();
         break;
     case 3:
-        blueberry_Smoothie->addDrink(3, numberOf);
-        totalPrice += blueberry_Smoothie->getAmount();
+        tempSmoothie.addDrink(3, numberOf);
+        *blueberry_Smoothie += tempSmoothie;
+        totalPrice += tempSmoothie.getAmount();
         break;
     case 4:
-        strawberry_Smoothie->addDrink(4, numberOf);
-        totalPrice += strawberry_Smoothie->getAmount();
+        tempSmoothie.addDrink(4, numberOf);
+        *strawberry_Smoothie += tempSmoothie;
+        totalPrice += tempSmoothie.getAmount();
         break;
     case 5:
-        sugarApple_Smoothie->addDrink(5, numberOf);
-        totalPrice += sugarApple_Smoothie->getAmount();
+        tempSmoothie.addDrink(5, numberOf);
+        *sugarApple_Smoothie += tempSmoothie;
+        totalPrice += tempSmoothie.getAmount();
         break;
     case 6:
-        mango_Smoothie->addDrink(6, numberOf);
-        totalPrice += mango_Smoothie->getAmount();
+        tempSmoothie.addDrink(6, numberOf);
+        *mango_Smoothie += tempSmoothie;
+        totalPrice += tempSmoothie.getAmount();
         break;
     }
 }
 
-void Customer::chooseYogurt(int thisDrink, int numberOf)
+void Table::chooseYogurt(int thisDrink, int numberOf)
 {
     switch (thisDrink)
     {
     case 1:
-        cheese_Yogurt->addDrink(1, numberOf);
-        totalPrice += cheese_Yogurt->getAmount();
+        tempYogurt.addDrink(1, numberOf);
+        *cheese_Yogurt += tempYogurt;
+        totalPrice += tempYogurt.getAmount();
         break;
     case 2:
-        avocado_Yogurt->addDrink(2, numberOf);
-        totalPrice += avocado_Yogurt->getAmount();
+        tempYogurt.addDrink(2, numberOf);
+        *avocado_Yogurt += tempYogurt;
+        totalPrice += tempYogurt.getAmount();
         break;
     case 3:
-        lychee_Yogurt->addDrink(3, numberOf);
-        totalPrice += lychee_Yogurt->getAmount();
+        tempYogurt.addDrink(3, numberOf);
+        *lychee_Yogurt += tempYogurt;
+        totalPrice += tempYogurt.getAmount();
         break;
     case 4:
-        mango_Yogurt->addDrink(4, numberOf);
-        totalPrice += mango_Yogurt->getAmount();
+        tempYogurt.addDrink(4, numberOf);
+        *mango_Yogurt += tempYogurt;
+        totalPrice += tempYogurt.getAmount();
         break;
     case 5:
-        blueberry_Yogurt->addDrink(5, numberOf);
-        totalPrice += blueberry_Yogurt->getAmount();
+        tempYogurt.addDrink(5, numberOf);
+        *blueberry_Yogurt += tempYogurt;
+        totalPrice += tempYogurt.getAmount();
         break;
     }
 }
 
-void Customer::chooseIceBlended(int thisDrink, int numberOf)
+void Table::chooseIceBlended(int thisDrink, int numberOf)
 {
     switch (thisDrink)
     {
     case 1:
-        lemon_IceBlended->addDrink(1, numberOf);
-        totalPrice += lemon_IceBlended->getAmount();
+        tempiceBlended.addDrink(1, numberOf);
+        *lemon_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     case 2:
-        blueberry_IceBlended->addDrink(2, numberOf);
-        totalPrice += blueberry_IceBlended->getAmount();
+        tempiceBlended.addDrink(2, numberOf);
+        *blueberry_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     case 3:
-        chocolate_IceBlended->addDrink(3, numberOf);
-        totalPrice += chocolate_IceBlended->getAmount();
+        tempiceBlended.addDrink(3, numberOf);
+        *chocolate_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     case 4:
-        mint_IceBlended->addDrink(4, numberOf);
-        totalPrice += mint_IceBlended->getAmount();
+        tempiceBlended.addDrink(4, numberOf);
+        *mint_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     case 5:
-        sapocheCoffee_IceBlended->addDrink(5, numberOf);
-        totalPrice += sapocheCoffee_IceBlended->getAmount();
+        tempiceBlended.addDrink(5, numberOf);
+        *sapocheCoffee_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     case 6:
-        matcha_IceBlended->addDrink(6, numberOf);
-        totalPrice += matcha_IceBlended->getAmount();
+        tempiceBlended.addDrink(6, numberOf);
+        *matcha_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     case 7:
-        strawberry_IceBlended->addDrink(7, numberOf);
-        totalPrice += strawberry_IceBlended->getAmount();
+        tempiceBlended.addDrink(7, numberOf);
+        *strawberry_IceBlended += tempiceBlended;
+        totalPrice += tempiceBlended.getAmount();
         break;
     }
 }
 
-void Customer::chooseSoda(int thisDrink, int numberOf)
+void Table::chooseSoda(int thisDrink, int numberOf)
 {
     switch (thisDrink)
     {
     case 1:
-        lemon_Soda->addDrink(1, numberOf);
-        totalPrice += lemon_Soda->getAmount();
+        tempSoda.addDrink(1, numberOf);
+        *lemon_Soda += tempSoda;
+        totalPrice += tempSoda.getAmount();
         break;
     case 2:
-        peach_Soda->addDrink(2, numberOf);
-        totalPrice += peach_Soda->getAmount();
+        tempSoda.addDrink(2, numberOf);
+        *peach_Soda += tempSoda;
+        totalPrice += tempSoda.getAmount();
         break;
     case 3:
-        mint_Soda->addDrink(3, numberOf);
-        totalPrice += mint_Soda->getAmount();
+        tempSoda.addDrink(3, numberOf);
+        *mint_Soda += tempSoda;
+        totalPrice += tempSoda.getAmount();
         break;
     case 4:
-        passion_Soda->addDrink(4, numberOf);
-        totalPrice += passion_Soda->getAmount();
+        tempSoda.addDrink(4, numberOf);
+        *passion_Soda += tempSoda;
+        totalPrice += tempSoda.getAmount();
         break;
     case 5:
-        kiwi_Soda->addDrink(5, numberOf);
-        totalPrice += kiwi_Soda->getAmount();
+        tempSoda.addDrink(5, numberOf);
+        *kiwi_Soda += tempSoda;
+        totalPrice += tempSoda.getAmount();
         break;
     case 6:
-        raspberry_Soda->addDrink(6, numberOf);
-        totalPrice += raspberry_Soda->getAmount();
+        tempSoda.addDrink(6, numberOf);
+        *raspberry_Soda += tempSoda;
+        totalPrice += tempSoda.getAmount();
         break;
     }
 }
 
-void Customer::chooseSoftDrink(int thisDrink, int numberOf)
+void Table::chooseSoftDrink(int thisDrink, int numberOf)
 {
     switch (thisDrink)
     {
     case 1:
-        sevenUp->addDrink(1, numberOf);
-        totalPrice += sevenUp->getAmount();
+        tempSoftDrink.addDrink(1, numberOf);
+        *sevenUp += tempSoftDrink;
+        totalPrice += tempSoftDrink.getAmount();
         break;
     case 2:
-        cocaCola->addDrink(2, numberOf);
-        totalPrice += cocaCola->getAmount();
+        tempSoftDrink.addDrink(2, numberOf);
+        *cocaCola += tempSoftDrink;
+        totalPrice += tempSoftDrink.getAmount();
         break;
     case 3:
-        redbull->addDrink(3, numberOf);
-        totalPrice += redbull->getAmount();
+        tempSoftDrink.addDrink(3, numberOf);
+        *redbull += tempSoftDrink;
+        totalPrice += tempSoftDrink.getAmount();
         break;
     case 4:
-        revive->addDrink(4, numberOf);
-        totalPrice += revive->getAmount();
+        tempSoftDrink.addDrink(4, numberOf);
+        *revive += tempSoftDrink;
+        totalPrice += tempSoftDrink.getAmount();
         break;
     case 5:
-        sting->addDrink(5, numberOf);
-        totalPrice += espresso->getAmount();
+        tempSoftDrink.addDrink(5, numberOf);
+        *sting += tempSoftDrink;
+        totalPrice += tempSoftDrink.getAmount();
         break;
     case 6:
-        c2->addDrink(6, numberOf);
-        totalPrice += c2->getAmount();
+        tempSoftDrink.addDrink(6, numberOf);
+        *c2 += tempSoftDrink;
+        totalPrice += tempSoftDrink.getAmount();
         break;
     }
 }
 
-void Customer::printIfExist()
+void Table::printIfExist()
 {
-	cout<<setw(30)<<"Ten"<<setw(20)<<"So luong"<<setw(20)<<"Don gia"<<setw(20)<<"Thanh tien"<<endl;
+    cout << setw(30) << "Ten" << setw(20) << "So luong" << setw(20) << "Don gia" << setw(20) << "Thanh tien" << endl;
     //TEA
     if (milkTea->getQuantity() != 0)
         milkTea->print();
@@ -631,9 +708,9 @@ void Customer::printIfExist()
         c2->print();
 }
 
-void Customer::removeAllDrink()
+void Table::removeAllDrink()
 {
-    for (int i = 0; i <= 10; i++)
+    for (int i = 1; i <= 10; i++)
     {
         chooseTea(i, 0);
         chooseCoffee(i, 0);
